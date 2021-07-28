62d733abea4c44fdfb2d71741e7ba57b55330c3d
Amine Grassi <grassi.amine@hotmail.fr>
2021-07-28 17:16:06 +0100

    creation des component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListPostComponent } from './list-post/list-post.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { UpdatePostComponent } from './update-post/update-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPostComponent,
+    AddPostComponent,
+    UpdatePostComponent
   ],
   imports: [
     BrowserModule,
